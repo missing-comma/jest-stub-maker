de6bf649a261dcf2e63b7cf0cb1f56797347bfde
AlexandreMPDias <alexandrempdias@icloud.com>
2022-12-24 22:37:47 -0500

    chore: add versioning

--- src/helpers/jester.ts
+++ src/helpers/jester.ts
@@ -2,22 +2,6 @@ type Foo = (...args: any) => any;
 
 export namespace Jester {
 	export type Mock<F extends Foo> = jest.Mock<ReturnType<F>, Parameters<F>>;
-
-	/** @deprecated */
-	export type Spy<F extends Foo> = jest.SpyInstance<ReturnType<F>, Parameters<F>>;
-	/** @deprecated */
-	export type Watchable<F extends Foo> = Mock<F>;
-
-	/** @deprecated */
-	export const asMock = <F extends Foo>(fn: F): Mock<F> => fn as any;
-
-	/** @deprecated */
-	export interface LifeCycles {
-		beforeAll: jest.Lifecycle;
-		beforeEach: jest.Lifecycle;
-		afterAll: jest.Lifecycle;
-		afterEach: jest.Lifecycle;
-	}
 }
 
 export namespace Jester.Mock {
